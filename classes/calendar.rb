91e2d57148af68a080e575371756175a83aa1bb9
Pete Dunlap <petedunlap@gmail.com>
2013-12-19 15:19:04 -0600

    calendar now determining how many days are in a given month in a given year

--- classes/calendar.rb
+++ classes/calendar.rb
@@ -1,15 +1,27 @@
 
 class Calendar
   attr_accessor :month, :year
+
   def initialize(input)
     if input.length == 2
-      @month = input[0]
-      @year = input[1]
+      @month = input[0].to_i
+      @year = input[1].to_i
+      checkYearInput
     elsif input.length == 1
-      @year = input[0]
+      @year = input[0].to_i
+      checkYearInput
+    end
+    @leap = false
+    if @year % 4 == 0
+      if @year % 100 == 0
+        @leap = true if @year % 400 == 0
+      else
+        @leap = true
+      end
     end
 
   end
+
   def zeller(day)
     zellerMonth = @month
     if @month == 1 or @month == 2
@@ -29,8 +41,16 @@ class Calendar
     quantity6 = 5 * century
     zellerDay = (quantity1 + quantity2 + quantity3 + quantity4 + quantity5 + quantity6) % 7
     (zellerDay + 6) % 7 #adjustment so that my scale begins with Sunday
-
   end
 
+  def days_in_month
+    days_by_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+    days_by_month[2 - 1] += 1 if @leap
+    days_by_month[@month - 1]
+  end
+
+  def checkYearInput
+    raise RangeError if @year < 1800 or @year > 3000
+  end
 
 end
--- test/test_cal.rb
+++ test/test_cal.rb
@@ -7,41 +7,88 @@ class TestCheersIntegration < MiniTest::Unit::TestCase
     # shell_output = `ruby cal.rb 2007`
     cal = Calendar.new([2007])
     expected_year = 2007
-    assert_equal cal.year, expected_year
+    assert_equal expected_year, cal.year
   end
 
   def test_calendar_class_can_take_year_and_month
     cal = Calendar.new([5,2007])
     expected_month = 5
     expected_year = 2007
-    assert_equal cal.month, expected_month
-    assert_equal cal.year, expected_year
+    assert_equal expected_month, cal.month
+    assert_equal expected_year, cal.year
   end
 
   def test_zeller_12_8_2013
     cal = Calendar.new([12,2013])
     expected_day_of_week = 0 #this means sunday
-    assert_equal cal.zeller(8), expected_day_of_week
+    assert_equal expected_day_of_week, cal.zeller(8)
   end
 
 
   def test_zeller_12_8_1800 #far past
     cal = Calendar.new([12,1800])
-    expected_day_of_week = 1 #this means sunday
-    assert_equal cal.zeller(8), expected_day_of_week
+    expected_day_of_week = 1
+    assert_equal expected_day_of_week, cal.zeller(8)
   end
 
   def test_zeller_3_1_3000 #far future
     cal = Calendar.new([3,3000])
-    expected_day_of_week = 6 #this means sunday
-    assert_equal cal.zeller(1), expected_day_of_week
+    expected_day_of_week = 6
+    assert_equal expected_day_of_week, cal.zeller(1)
   end
 
   def test_zeller_2_29_2040 #leap day
     cal = Calendar.new([2,2040])
-    expected_day_of_week = 3 #this means sunday
-    assert_equal cal.zeller(29), expected_day_of_week
+    expected_day_of_week = 3
+    assert_equal expected_day_of_week, cal.zeller(29)
   end
+
+  def test_chooses_correct_number_of_days_in_month
+    cal = Calendar.new([9,2013])
+    expected_days_in_month = 30
+    assert_equal expected_days_in_month, cal.days_in_month
+  end
+
+  def test_chooses_correct_number_of_days_in_month_non_leap_year
+    cal = Calendar.new([2,2013])
+    expected_days_in_month = 28
+    assert_equal expected_days_in_month, cal.days_in_month
+  end
+
+  def test_chooses_correct_number_of_days_in_month_leap_year
+    cal = Calendar.new([2,2004])
+    expected_days_in_month = 29
+    assert_equal expected_days_in_month, cal.days_in_month
+  end
+
+  def test_chooses_correct_number_of_days_in_month_century_leap_year
+    cal = Calendar.new([2,2000])
+    expected_days_in_month = 29
+    assert_equal expected_days_in_month, cal.days_in_month
+  end
+
+  def test_chooses_correct_number_of_days_in_month_non_century_leap_year
+    cal = Calendar.new([2,1900])
+    expected_days_in_month = 28
+    assert_equal expected_days_in_month, cal.days_in_month
+  end
+
+  def test_refuses_year_before_1800
+    assert_raises RangeError do
+      cal = Calendar.new([1799])
+    end
+  end
+
+  def test_refuses_year_after_3000
+    assert_raises RangeError do
+      cal = Calendar.new([3001])
+    end
+  end
+
+
+
+
+
 
 
 
@@ -57,7 +104,7 @@ class TestCheersIntegration < MiniTest::Unit::TestCase
 # BRT's just GRAND!
 # Your name backwards is trb
 # EOS
-#     assert_equal shell_output, expected_output
+#     assert_equal expected_output, shell_output
 #   end
 
 #   def test_reverse_your_name
@@ -72,48 +119,5 @@ class TestCheersIntegration < MiniTest::Unit::TestCase
 
 end
 
-# use string =~ /sdfkjljk/
-# this takes a string and returns true or false if it is match according to regex
 
 
-
-
-#   def test_a_name_with_no_vowels
-#     shell_output = ""
-#     IO.popen('ruby cheers.rb', 'r+') do |pipe|
-#       pipe.puts("brt")
-#       pipe.close_write
-#       shell_output = pipe.read
-#     end
-#     expected_output = <<EOS
-# What's your name?
-# Give me a... B
-# Give me a... R
-# Give me a... T
-# BRT's just GRAND!
-# Your name backwards is trb
-# EOS
-#     assert_equal shell_output, expected_output
-#   end
-
-#   def test_works_for_vowels
-#     shell_output = ""
-#     IO.popen('ruby cheers.rb', 'r+') do |pipe|
-#       pipe.puts("poppa")
-#       pipe.close_write
-#       shell_output = pipe.read
-#     end
-#     expected_output = <<EOS
-# What's your name?
-# Give me a... P
-# Give me an... O
-# Give me a... P
-# Give me a... P
-# Give me an... A
-# POPPA's just GRAND!
-# Your name backwards is appop
-# EOS
-#     assert_equal shell_output, expected_output
-#   end
-
-# #   # But what about names with vowels?!!
